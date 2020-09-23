069b5021ebe0698c211e2ab1e7cd510b51d951f6
Christian Duerr <contact@christianduerr.com>
2020-09-23 03:14:23 +0200

    Use `alacritty --version` instead of commit hash

--- alacritty_master.sh
+++ alacritty_master.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 user="perfbot"
-hashfile="./lasthash"
+versionfile="./lastversion"
 vtebench_path="/home/$user/vtebench"
 alacritty_path="/home/$user/alacritty"
 output_directory="results/alacritty/master"
@@ -18,7 +18,6 @@ if ! id "$user" &> /dev/null; then
     exit 12
 fi
 
-# Run as much as possible without root permissons.
 # Clone Alacritty if it does not exist yet.
 if ! [ -d "$alacritty_path" ]; then
     sudo -u $user git clone https://github.com/alacritty/alacritty "$alacritty_path"
@@ -27,17 +26,17 @@ fi
 # Update to the latest version of Alacritty.
 sudo -u $user git -C "$alacritty_path" pull origin master --rebase
 
-# Check if there has been any new commit.
-hash=$(git -C "$alacritty_path" rev-parse HEAD)
-lasthash=$(cat "$hashfile")
-if [ "$lasthash" = "$hash" ]; then
+# Build the latest version.
+sudo -u $user cargo build --release --manifest-path "$alacritty_path/Cargo.toml"
+
+# Check if there has been any new changes.
+version=$("$alacritty_path/target/release/alacritty" --version)
+lastversion=$(cat "$versionfile")
+if [ "$lastversion" = "$version" ]; then
     echo "No new commits to test"
     exit
 fi
-printf "$hash" | sudo -u $user tee "$hashfile"
-
-# Build the latest version.
-sudo -u $user cargo build --release --manifest-path "$alacritty_path/Cargo.toml"
+printf "$lastversion" | sudo -u $user tee "$versionfile"
 
 ./bench.sh "$alacritty_path/target/release/alacritty" "$output_directory"
 
@@ -47,6 +46,6 @@ sudo -u $user cargo build --release --manifest-path "$alacritty_path/Cargo.toml"
 # Push changes to GitHub.
 shorthash=$(git -C "$alacritty_path" rev-parse --short HEAD)
 message=$(date +"Add results for Alacritty master ($shorthash)")
-sudo -u $user git add "$hashfile" results/alacritty/master
+sudo -u $user git add "$versionfile" results/alacritty/master
 sudo -u $user git commit -m "$message"
 sudo -u $user git push origin master
