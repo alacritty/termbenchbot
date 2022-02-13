06463ac926c57ee1ecb14ce9731e5dd60d7b3656
Christian Duerr <contact@christianduerr.com>
2022-02-13 06:29:25 +0100

    Delete job results without payload
    If a job result was submitted for the master branch, it does not contain
    any payload. However it should still remove the job from the job queue
    to prevent re-execution.

--- src/webserver.rs
+++ src/webserver.rs
@@ -28,23 +28,29 @@ fn mark_started(id: i32) {
 
 /// Submit benchmark results.
 #[post("/jobs/<id>", data = "<data>")]
-fn submit_results(id: i32, data: Option<Json<BenchmarkResults>>) -> (Status, &str) {
+fn submit_results(id: i32, data: Option<Json<BenchmarkResults>>) -> (Status, String) {
     let connection = model::db_connection();
 
     let job = match Job::from_id(&connection, id) {
         Some(job) if job.started_at.is_some() => job,
-        Some(_) => return (Status::BadRequest, "job must be marked as running first"),
-        None => return (Status::NotFound, ""),
+        Some(_) => return (Status::BadRequest, "job must be marked as running first".into()),
+        None => return (Status::NotFound, "".into()),
     };
 
-    if let (Some(comments_url), Some(data)) = (&job.comments_url, data) {
-        match Comment::new(comments_url, data.result) {
-            Ok(_) => job.delete(&connection),
-            Err(_) => job.mark_pending(&connection),
+    // Attempt to write results to the GitHub thread they were requested in.
+    //
+    // On failure, we stage the request for re-execution to attempt upload again at a later point
+    // in time after potential network issues have been resolved.
+    if let Some((comments_url, data)) = job.comments_url.as_ref().zip(data) {
+        if let Err(err) = Comment::new(&comments_url, data.result) {
+            job.mark_pending(&connection);
+            return (Status::InternalServerError, err.to_string());
         }
     }
 
-    (Status::Ok, "")
+    job.delete(&connection);
+
+    (Status::Ok, "".into())
 }
 
 /// Start the webserver.
