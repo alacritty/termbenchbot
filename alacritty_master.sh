d76533d3423aabf8d3577afdd6e214e95e2f6f3a
Christian Duerr <contact@christianduerr.com>
2024-12-28 21:22:17 +0100

    Fix graphs generation with column mismatch
    With a mismatch in columns, gnuplot would previously generate
    nonsensical graphs containing multiple columns into a single column.
    
    This patch fixes this issue by filtering the latest 10 bencharks to
    ignore all files which have less columns than the benchmark with the
    most columns.
    
    Theoretically it would still be possible to create issues by adding and
    removing a benchmark in under 10 master commits, which would cause the
    results to appear in an unrelated column. However since this is unlikely
    and unnecessary at the moment, it's not considered for this PR.

--- alacritty_master.sh
+++ alacritty_master.sh
@@ -43,8 +43,25 @@ printf "$version" | sudo -u $user tee "$versionfile"
 
 # Update the plots.
 mkdir -p "$output_directory/charts/"
-"$vtebench_path/gnuplot/summary.sh" $(ls results/alacritty/master/*.dat | tail -n 10) "$output_directory/charts/summary.svg"
-"$vtebench_path/gnuplot/detailed.sh" $(ls results/alacritty/master/*.dat | tail -n 3) "$output_directory/charts/"
+max_cols=0
+latest_ten=""
+for file in $(ls results/alacritty/master/*.dat | tail -n 10); do
+    # Get number of columns in the data file.
+    cols=$(head -n 1 "$file" | awk '{print NF}')
+
+    # Clear all files if we find one with more columns.
+    if [ $cols -gt $max_cols ]; then
+        max_cols=$cols
+        latest_ten=""
+    fi
+
+    # With matching columns, add this file to the list.
+    if [ $cols -eq $max_cols ]; then
+        latest_ten=$(printf "$latest_ten\n$file")
+    fi
+done
+"$vtebench_path/gnuplot/summary.sh" $(echo $latest_ten) "$output_directory/charts/summary.svg"
+"$vtebench_path/gnuplot/detailed.sh" $(echo $latest_ten | tail -n 3) "$output_directory/charts/"
 
 # Push changes to GitHub.
 shorthash=$(sudo -u $user git -C "$alacritty_path" rev-parse --short HEAD)
